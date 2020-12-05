ab74ecb45fcb5a6fd926e73418dcac1d4b7fd586
Artem Yurchenko <artem@studiovector.pl>
2020-12-05 17:38:28 +0100

    Init commit

---
+++ WMR-USB-Controller/WMR-USB-Controller/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace WMR_USB_Controller
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
