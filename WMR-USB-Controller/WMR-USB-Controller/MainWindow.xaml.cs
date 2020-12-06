5928f45b25d38d4551139cd082e3c45394eb806f
Artem Yurchenko <artem@studiovector.pl>
2020-12-06 10:38:32 +0100

    Tray icon was implemented

--- WMR-USB-Controller/WMR-USB-Controller/MainWindow.xaml.cs
+++ WMR-USB-Controller/WMR-USB-Controller/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
+using WMR_USB_Controller.YUART.Tray_Icon;
 using WMR_USB_Controller.YUART.USB;
 
 namespace WMR_USB_Controller
@@ -9,20 +11,47 @@ namespace WMR_USB_Controller
     public partial class MainWindow
     {
         private readonly UsbDevicesManager _usbDevicesManager = new UsbDevicesManager();
-
+        private TrayIconManager _trayIconManager;
+        
         public MainWindow()
         {
             InitializeComponent();
-            
+
+            SetupTrayIconManager();
+
             _usbDevicesManager.Initialize();
         }
-        
-        private void HandleCheck(object sender, RoutedEventArgs e) { 
-            _usbDevicesManager.ActivateWmrDevice(false); 
+
+        private void SetupTrayIconManager()
+        {
+            _trayIconManager = new TrayIconManager(this);
+            _trayIconManager.Initialize();
+        }
+
+        protected override void OnStateChanged(EventArgs e)
+        {
+            if (WindowState == WindowState.Minimized)
+            {
+                Hide();
+                _trayIconManager.ShowIcon();
+            }
+
+            base.OnStateChanged(e);
         }
         
-        private void UnhandleCheck(object sender, RoutedEventArgs e) { 
-            _usbDevicesManager.ActivateWmrDevice(true); 
+        private void DisableWmrDeviceAction(object sender, RoutedEventArgs e)
+        {
+            ChangeWmrDeviceState(false);
+        }
+
+        private void EnableWmrDeviceAction(object sender, RoutedEventArgs e)
+        {
+            ChangeWmrDeviceState(true);
+        }
+
+        public void ChangeWmrDeviceState(bool newState)
+        {
+            _usbDevicesManager.ActivateWmrDevice(newState);
         }
     }
 }
---
+++ WMR-USB-Controller/WMR-USB-Controller/YUART/Tray Icon/TrayIconManager.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Forms;
+
+namespace WMR_USB_Controller.YUART.Tray_Icon
+{
+    /// <summary>
+    /// Class, that manages behaviour of the application in tray.
+    /// </summary>
+    public sealed class TrayIconManager
+    {
+        private const string PathToTrayIcon = "Main.ico";
+        private const string ToolTipText = "WMR USB Controller";
+        private const string DisableWmrMenuItemText = "Disable WMR Device";
+        private const string EnableWmrMenuItemText = "Enable WMR Device";
+
+        private readonly NotifyIcon _trayIcon = new NotifyIcon
+        {
+            Icon = new System.Drawing.Icon(PathToTrayIcon),
+            Visible = false,
+            Text = ToolTipText
+        };
+        
+        private readonly MainWindow _mainWindow;
+        
+        private ContextMenu _disableWmrMenu;
+        private ContextMenu _enableWmrMenu;
+
+        public TrayIconManager(MainWindow mainWindow)
+        {
+            _mainWindow = mainWindow;
+        }
+
+        /// <summary>
+        /// Initialize behaviour of tray icon.
+        /// </summary>
+        public void Initialize()
+        {
+            InitializeTrayOptions();
+            
+            _trayIcon.ContextMenu = _disableWmrMenu;
+
+            _trayIcon.DoubleClick += ShowMainWindow;
+        }
+
+        private void ShowMainWindow(object _, EventArgs eventArgs)
+        {
+            HideIcon();
+            _mainWindow.Show();
+            _mainWindow.WindowState = WindowState.Normal;
+        }
+
+        private void HideIcon()
+        {
+            _trayIcon.Visible = false;
+        }
+
+        private void InitializeTrayOptions()
+        {
+            _disableWmrMenu = new ContextMenu(new[]
+            {
+                new MenuItem(DisableWmrMenuItemText, DisableWmr)
+            });
+
+            _enableWmrMenu = new ContextMenu(new[]
+            {
+                new MenuItem(EnableWmrMenuItemText, ActivateWmr)
+            });
+        }
+
+        /// <summary>
+        /// Makes icon visible in the tray.
+        /// </summary>
+        public void ShowIcon()
+        {
+            _trayIcon.Visible = true;
+        }
+
+        private void DisableWmr(object _, EventArgs eventArgs)
+        {
+            _trayIcon.ContextMenu = _enableWmrMenu;
+            _mainWindow.ChangeWmrDeviceState(false);
+        }
+
+        private void ActivateWmr(object _, EventArgs eventArgs)
+        {
+            _trayIcon.ContextMenu = _disableWmrMenu;
+            _mainWindow.ChangeWmrDeviceState(true);
+        }
+    }
+}
