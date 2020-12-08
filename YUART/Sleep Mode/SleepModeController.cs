45c7acfd95fcaaaa4ed0ab5445ebeded32e3f154
Artem Yurchenko <artem@studiovector.pl>
2020-12-08 15:18:34 +0100

    Sleep mode controller tab was added; SleepModeController class was added

---
+++ YUART/Sleep Mode/SleepModeController.cs
@@ -0,0 +1,10 @@
+﻿namespace WMR_USB_Controller.YUART.Sleep_Mode
+{
+    /// <summary>
+    /// Class, that provide controls for sleep mode of WMR.
+    /// </summary>
+    public sealed class SleepModeController
+    {
+        
+    }
+}
